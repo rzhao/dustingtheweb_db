ec8a6fd7ec6234f9795db26b58606c9f679df6c4
Richard Zhao <rzhao15@gmail.com>
2012-03-13 15:42:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrawlerDb::Application.config.secret_token = 'c913df94f49625747ecb4950cc0b4c5b6be4edea425e55c1dc8643656069f957b5e36a4e0889b0f37c20425eb5c03c6549974453316f846f0f6172c68bbd11b7'
