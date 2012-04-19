818615b07fb3af2cbcdb832240f86b5858d747c9
Richard Zhao <rzhao15@gmail.com>
2012-04-18 20:59:43 -0700

    added dump and can now send vulnerability name

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,14 +9,28 @@ class MessagesController < ApplicationController
   end
   
   def create
+    vid = params[:message][:vulnerability_id]
+    if vid.to_i == 0
+      vid = Vulnerability.find_by_name(vid).id rescue nil
+    end
+
+    params[:message][:vulnerability_id] = vid unless vid.nil?
     @message = Message.new(params[:message])
 
     respond_to do |format|
-      if @message.save
+      if vid.nil?
+        format.json { render json: { "Error: Improper vulnerability_id" }, status: :unprocessable_entity }
+      elsif @message.save
         format.json { render json: @message, status: :created, location: @message }
       else
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
+    end
+  end
+  
+  def dump
+    respond_to do |format|
+      format.json { render json: Message.all.to_json }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ CrawlerDb::Application.routes.draw do
   resources :vulnerabilities
 
   resources :messages, :only => [:index, :create, :destroy]
+  
+  match 'dump' => 'index#dump'
 
   root :to => 'index#index'
 end
